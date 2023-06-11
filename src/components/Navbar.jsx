0b54e4c5eb31ed423f8cc2e0bf277f3848a9cd0a
Neville Brem <contact@nevillebrem.com>
2023-06-11 19:40:54 +0200

    added icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import Icon from '../assets/favicon.png'
 
-const Navbar = ({ auth }) => {
+const Navbar = () => {
 
   return (
-    <div className='w-full bg-slate-700 text-white flex justify-between p-4 text-lg'>
-        <a href="/"><FontAwesomeIcon icon={faHome} className='text-4xl my-auto' /></a>
+    <div className='w-full bg-slate-700 text-slate-100 flex justify-between p-4 text-lg'>
+        <a href="/"><img src={Icon} alt="icon" className='h-20 bg-slate-100 p-1 rounded-2xl' /></a>
         <div className='text-xl my-auto font-extrabold flex gap-8'>
           <a href="/create">Create Post</a>
           <a href="/account">Account</a>
