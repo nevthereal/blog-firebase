f5bb59bd454e50e45918874b4ab60eadc01b24aa
Neville Brem <contact@nevillebrem.com>
2023-06-11 17:21:55 +0200

    nav layout change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
 
 const Navbar = ({ auth }) => {
-  const [user, loading] = useAuthState(auth);
 
   return (
     <div className='w-full bg-slate-700 text-white flex justify-between p-4 text-lg'>
         <a href="/"><FontAwesomeIcon icon={faHome} className='text-4xl my-auto' /></a>
-        <div className='text-xl my-auto font-extrabold flex gap-2'>
-          <a href="/create">Create</a>
+        <div className='text-xl my-auto font-extrabold flex gap-8'>
+          <a href="/create">Create Post</a>
           <a href="/account">Account</a>
         </div>
     </div>
