df4647498c113baf3d2cf2011afa2e241a820b12
Neville Brem <contact@nevillebrem.com>
2023-06-12 20:54:46 +0200

    implemented query, time and order not fixed yet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import timestamp from 'time-stamp'
 import Post from './Post'
+import { query, collection, onSnapshot, orderBy, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const Home = ({ auth }) => {
 
   const [user, loading] = useAuthState(auth)
+  const [postList, setPostList] = useState([])
+  
 
   const hour = timestamp('HH')
   let dayTime;
@@ -15,16 +19,27 @@ const Home = ({ auth }) => {
   } else {
     dayTime = "Evening"
   }
-
+  
+  useEffect(() => {
+    const q = query(collection(db, 'posts'), orderBy('time'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) =>{
+      let postsArr = []
+      querySnapshot.forEach((doc) => {
+        postsArr.push({...doc.data(), id:doc.id})
+      });
+      setPostList(postsArr)
+    })
+    return () => unsubscribe()
+  }, [])
   return (
     <div className='m-10'>
-      {user ? <h1 className='font-bold text-slate-900 text-6xl'>Good {dayTime}, {user.displayName}</h1> : loading? <></> : <></>}
+      {user ? <h1 className='font-bold text-slate-900 text-5xl'>Good {dayTime}, {user.displayName}</h1> : loading? <></> : <></>}
       <p className='font-bold text-2xl pt-2 pb-4 text-slate-800'>Here are the latest posts:</p>
-      <div>
-        <Post />
-        <Post />
-        <Post />
-      </div>
+          {postList.map((post, index) => {
+            return (
+              <Post key={index} title={post.title} pfp={post.pfp} author={post.author} content={post.content} />
+            )
+          })}
     </div>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import Logo from '../assets/favicon.png'
 
-const Post = () => {
-  
-  const style = {
-    info: `font-bold`,
-    subText: `text-slate-600`,
-    heading: `text-3xl font-bold`
-  }
+const style = {
+  info: `font-bold`,
+  subText: `text-slate-600`,
+  heading: `text-3xl font-bold`
+}
+
+const Post = ({ title, pfp, author, content, time }) => {
   
   return (
     <div className='bg-slate-200 p-4 my-4 rounded-xl border border-slate-300 shadow-lg'>
-      <h1 className={style.heading}>Hello World!</h1>
-      <div className='flex gap-1'>
-        <img src={Logo} alt="" className='h-8' /><p className='text-slate-700 text-xl my-auto'>Neville Brem</p>
+      <h1 className={style.heading}>{title}</h1>
+      <div className='flex gap-2'>
+        <img src={pfp} alt="" className='h-7 rounded-full' /><p className='text-slate-700 text-xl my-auto'>{author}</p>
       </div>
-      <p>This is just a little test, if this works, I am very happy!</p>
-      <p className={style.subText}>Created at: <span className={style.info}>11 Jun 19:51</span></p>
+      <p>{content}</p>
+      <p className={style.subText}>Created at: <span className={style.info}>{time}</span></p>
     </div>
   )
 }
