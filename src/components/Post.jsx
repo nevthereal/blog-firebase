0e2ce490b6b5fd9850bf83880054e5dec3361db5
Neville Brem <contact@nevillebrem.com>
2023-06-22 19:40:20 +0200

    remade post design

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 
-const style = {
-  info: `font-bold`,
-  subText: `text-slate-600`,
-  heading: `text-3xl font-bold`,
-}
-
 const Post = ({ title, pfp, author, content, time }) => {
   
   return (
-    <div className='bg-slate-200 p-4 my-4 rounded-xl border border-slate-300 shadow-lg'>
-      <h1 className={style.heading}>{title}</h1>
-      <div className='flex gap-2'>
-        <img src={pfp} alt="" className='h-7 rounded-full' /><p className='text-slate-700 text-xl my-auto'>{author}</p>
-      </div>
-      <p dangerouslySetInnerHTML={{__html:content}}></p>
-      <p className={style.subText}>Created at: <span className={style.info}>{time}</span></p>
+  <div className="bg-slate-200 p-4 my-4 rounded-xl border border-slate-300 shadow-lg">
+    <p className="text-sm mt-2 text-gray-600">{time}</p>
+    <h1 className="text-2xl font-bold mb-4">{title}</h1>
+    <div className="flex items-center mb-4">
+      <img src={pfp} alt="" className="h-7 rounded-full mr-2" />
+      <p className="text-slate-700 text-lg">{author}</p>
     </div>
+      <p className="text-gray-800" dangerouslySetInnerHTML={{ __html: content }}></p>
+      
+  </div>
   )
 }
 
