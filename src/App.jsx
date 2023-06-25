bc0e1b635511c2c6af0a0305110d71482fff6d69
Neville Brem <contact@nevillebrem.com>
2023-06-25 17:15:53 +0200

    added post delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         signOut(auth).then(() => {
         }).catch(() => {
         });      
-    }) 
+    })
 
   return (
     <>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { db } from '../firebase';
+import { doc, deleteDoc } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import React from 'react';
 
-const Post = ({ title, pfp, author, content, time, email }) => {
+const Post = ({ title, pfp, author, content, time, email, id, auth }) => {
+  const [user, loading] = useAuthState(auth);
+
+  const deletePost = async (postId) => {
+    await deleteDoc(doc(db, 'posts', postId));
+  };
 
   return (
-  <div className="bg-slate-200 p-4 my-4 rounded-xl border border-slate-300 shadow-lg">
-    <p className="text-sm mt-2 text-gray-600">{time}</p>
-    <h1 className="text-2xl font-bold mb-4">{title}</h1>
-    <div className="flex items-center mb-4">
-      <img src={pfp} alt="" className="h-7 rounded-full mr-2" />
-      <p className="text-slate-700 text-lg">{author} {email == "neville.brem@gmail.com" ? <span>(admin)</span> : <></>}</p>
+    <div className="bg-slate-200 p-4 my-4 rounded-xl border border-slate-300 shadow-lg flex justify-between">
+      <div>
+        <p className="text-sm mt-2 text-gray-600">{time}</p>
+        <h1 className="text-2xl font-bold mb-4">{title}</h1>
+        <div className="flex items-center mb-4">
+          <img src={pfp} alt="" className="h-7 rounded-full mr-2" />
+          <p className="text-slate-700 text-lg">
+            {author} {email === 'neville.brem@gmail.com' ? <span>(admin)</span> : null}
+          </p>
+        </div>
+        <p className="text-gray-800" dangerouslySetInnerHTML={{ __html: content }}></p>
+      </div>
+      {!loading && user && user.email === email ? (
+        <button onClick={() => deletePost(id)}>
+          <FontAwesomeIcon icon={faTrashAlt} className="text-2xl text-slate-700" />
+        </button>
+      ) : null}
     </div>
-      <p className="text-gray-800" dangerouslySetInnerHTML={{ __html: content }}></p>
-      
-  </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
