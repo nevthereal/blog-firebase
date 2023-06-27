c9668276919246e12ca8a7721e61e2a4c4080914
Neville Brem <contact@nevillebrem.com>
2023-06-27 20:49:00 +0200

    navbar and favicon change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Icon from '../assets/favicon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
 
   return (
     <div className='w-full bg-slate-700 text-slate-100 flex justify-between p-4 text-lg'>
-        <a href="/"><img src={Icon} alt="icon" className='h-16 bg-slate-100 p-1 rounded-2xl' /></a>
+        <a href="/"><FontAwesomeIcon icon={faNetworkWired} className='text-white no-underline text-4xl md:text-5xl' /></a>
         <div className='text-xl my-auto font-extrabold flex gap-8'>
           <a href="/create" className='text-white no-underline'>Create Post</a>
           <a href="/account" className='text-white no-underline'>Account</a>
